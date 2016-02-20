d2992290915ae1dc79414b3ae767ea5f95a0af68
Onneby Peter <peter.onneby@altares.fr>
2016-02-20 23:29:45 +0100

    Save User

--- src/test/java/com/onneby/dynamodb/CreateTableTest.java
+++ src/test/java/com/onneby/dynamodb/CreateTableTest.java
@@ -8,15 +8,17 @@ import com.amazonaws.services.dynamodbv2.local.main.ServerRunner;
 import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.UUID;
 
 /**
  * Created by ponneby on 20/02/16.
  */
 public class CreateTableTest {
-    @Before
-    public void startAnInMemoryServer() throws Exception {
+    @BeforeClass
+    public static void startAnInMemoryServer() throws Exception {
         final String[] localArgs = {"-inMemory", "-port", "30000", "-sharedDb"};
 
         final DynamoDBProxyServer server = ServerRunner.createServerFromCommandLineArgs(localArgs);
@@ -42,7 +44,19 @@ public class CreateTableTest {
 
     }
 
+    @Test
+    public void saveItem() throws Exception {
+        AmazonDynamoDBClient dynamoDbClient = new AmazonDynamoDBClient();
+        dynamoDbClient.setEndpoint("http://localhost:30000");
+        DynamoDBMapperConfig config = new DynamoDBMapperConfig(DynamoDBMapperConfig.DEFAULT, new DynamoDBMapperConfig(ConversionSchemas.V2));
+        final DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(dynamoDbClient, config);
+        CreateTableRequest req = dynamoDBMapper.generateCreateTableRequest(User.class);
+        req.setProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(1L).withWriteCapacityUnits(1L));
 
+        User user = new User();
+        user.setId(UUID.randomUUID().toString());
+        dynamoDBMapper.save(user);
+    }
 
     @DynamoDBTable(tableName = "User")
     public class User {
