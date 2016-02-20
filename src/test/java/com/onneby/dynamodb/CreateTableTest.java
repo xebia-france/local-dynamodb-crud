a27c3aaa0a87741a80be1135ae2169fdd681171f
Onneby Peter <peter.onneby@altares.fr>
2016-02-20 23:02:00 +0100

    Try to create a table with no annotations

--- src/test/java/com/onneby/dynamodb/CreateTableTest.java
+++ src/test/java/com/onneby/dynamodb/CreateTableTest.java
@@ -1,14 +1,22 @@
 package com.onneby.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.ConversionSchemas;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.local.main.ServerRunner;
 import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by ponneby on 20/02/16.
  */
 public class CreateTableTest {
-    @Test
+    @Before
     public void startAnInMemoryServer() throws Exception {
         final String[] localArgs = {"-inMemory", "-port", "30000"};
 
@@ -18,5 +26,20 @@ public class CreateTableTest {
 
     }
 
-    
+    @Test
+    public void createTable() throws Exception {
+
+        DynamoDBMapperConfig config = new DynamoDBMapperConfig(DynamoDBMapperConfig.DEFAULT, new DynamoDBMapperConfig(ConversionSchemas.V2));
+        AmazonDynamoDBClient dynamoDbClient = new AmazonDynamoDBClient();
+        final DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(dynamoDbClient, config);
+        CreateTableRequest req = dynamoDBMapper.generateCreateTableRequest(User.class);
+
+        DynamoDB dynamoDB = new DynamoDB(dynamoDbClient);
+        Table table = dynamoDB.createTable(req);
+        table.waitForActive();
+
+    }
+
+    public class User {
+    }
 }
